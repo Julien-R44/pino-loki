040731e977ec60188f742638abbd824bdb5a57c1
Julien <julien@ripouteau.com>
2022-02-21 07:51:31 +0100

    style: add some docs

--- src/loki.js
+++ src/loki.js
@@ -4,66 +4,78 @@ const axios = require('axios')
 const stream = require('stream')
 
 class Client {
-  constructor (options = {}) {
+  constructor(options = {}) {
     this._options = options
   }
 
-  async insert (items = []) {
+  async insert(items = []) {
     const data = Array.isArray(items) ? items : [items]
     if (data.length <= 0) {
       return
     }
 
-    data.forEach(async item => {
+    data.forEach(async (item) => {
       const url = `${this._options.hostname}/loki/api/v1/push`
 
-      Object.keys(item.tags).map(function(key, index) {
-        item.tags[key] = item.tags[key].toString();
+      Object.keys(item.tags).map(function (key, index) {
+        item.tags[key] = item.tags[key].toString()
       })
 
-      const toSend = { 
-        streams: [{
-          stream: {
-            application: this._options.applicationTag,
-            level: item.status,
-            ...item.tags
-          },
-          values: [[
-            (new Date().getTime() * 1000000).toString(),
-            JSON.stringify(item.data)
-          ]]
-        }]
-      };
-      
+      const toSend = {
+        streams: [
+          {
+            stream: {
+              application: this._options.applicationTag,
+              level: item.status,
+              ...item.tags
+            },
+            values: [[(new Date().getTime() * 1000000).toString(), JSON.stringify(item.data)]]
+          }
+        ]
+      }
+
       try {
-        return await axios.post(url, toSend, { 
+        return await axios.post(url, toSend, {
           timeout: this._options.timeout || 1800000, // Timeout after 30 minutes
           headers: { 'Content-Type': 'application/json' }
         })
-      } catch(err){
-        if(this._options.errorHandler){
+      } catch (err) {
+        /**
+         * If the the user has defined a custom error handler,
+         * we will try to call it
+         */
+        if (this._options.errorHandler) {
           try {
-            this._options.errorHandler(err);
-          } catch(customHandlerError){
-            console.error("Got error from custom handler! Output:", customHandlerError);
+            this._options.errorHandler(err)
+          } catch (customHandlerError) {
+            console.error('Got error from custom handler! Output:', customHandlerError)
           }
         }
-        if(this._options.silenceErrors !== true){
-          if(err.response){
-            console.error(`Attempting to send log to Loki failed with status '${err.response.status}: ${err.response.statusText}' returned reason: ${err.response.data.trim()}`);
-          } else if(err.isAxiosError === true){
-            console.error(`Attempting to send log to Loki failed. Got an axios error, error code: '${err.code}' message: ${err.message}`);
+
+        /**
+         * If the user has defined silenceErrors, we will not
+         * show any errors
+         */
+        if (this._options.silenceErrors !== true) {
+          if (err.response) {
+            console.error(
+              `Attempting to send log to Loki failed with status '${err.response.status}: ${
+                err.response.statusText
+              }' returned reason: ${err.response.data.trim()}`
+            )
+          } else if (err.isAxiosError === true) {
+            console.error(
+              `Attempting to send log to Loki failed. Got an axios error, error code: '${err.code}' message: ${err.message}`
+            )
           } else {
-            console.error('Got unknown error when trying to send log to Loki, error output:', err);
+            console.error('Got unknown error when trying to send log to Loki, error output:', err)
           }
         }
-        
-        
       }
     })
   }
 
-  insertStream () {
+  insertStream() {
     const self = this
     const writeStream = new stream.Writable({
       objectMode: true,
