95fcee8e3f7a979526b73975b9f65478ed832c1c
Julien <julien@ripouteau.com>
2023-01-21 14:13:25 +0100

    fix: add shebang for cli entries
    Fixes #9

--- build.config.ts
+++ build.config.ts
@@ -1,4 +1,5 @@
 import { defineBuildConfig } from 'unbuild'
+import { readFileSync, writeFileSync } from 'fs'
 
 export default defineBuildConfig({
   entries: ['src/index', 'src/cli'],
@@ -7,5 +8,18 @@ export default defineBuildConfig({
   clean: true,
   rollup: {
     emitCJS: true,
+  },
+  hooks: {
+    'build:done': () => {
+      /**
+       * Prepend shebang to cli entries since unbuild doesn't seem to support it
+       */
+      const cliEntries = ['dist/cli.mjs', 'dist/cli.cjs']
+
+      for (const entry of cliEntries) {
+        const content = `#!/usr/bin/env node \n${readFileSync(entry, 'utf-8')}`
+        writeFileSync(entry, content)
+      }
+    },
   },
 })
