29127db619a10c5d92059084c5271ebb463d8e65
Julien Ripouteau <julien@ripouteau.com>
2024-05-12 00:57:37 +0200

    refactor: migrate to commander 12

--- src/cli.ts
+++ src/cli.ts
@@ -5,37 +5,28 @@ import build from './index'
 import pkg from '../package.json'
 import type { LokiOptions } from './types/index'
 
-/**
- * Parse cli arguments with commander
- */
-export const parseArgs = () => {
-  program
-    .version(pkg.version)
-    .option('-u, --user <user>', 'Loki username')
-    .option('-p, --password <password>', 'Loki password')
-    .option('--hostname <hostname>', 'URL for Loki')
-    .option('-b, --batch', 'Should logs be sent in batch mode')
-    .option('-i, --interval <interval>', 'The interval at which batched logs are sent in seconds')
-    .option('-t, --timeout <timeout>', 'Timeout for request to Loki')
-    .option('-s, --silenceErrors', 'If false, errors will be displayed in the console')
-    .option('-r, --replaceTimestamp', 'Replace pino logs timestamps with Date.now()')
-    .option('-l, --labels <label>', 'Additional labels to be added to all Loki logs')
-    .option(
-      '-pl, --propsLabels <labels>',
-      'Fields in log line to convert to Loki labels (comma separated values)',
-    )
-    .option('--no-stdout', 'Disable output to stdout')
-
-  program.parse(process.argv)
-
-  return program.opts()
-}
+program
+  .version(pkg.version)
+  .option('-u, --user <user>', 'Loki username')
+  .option('-p, --password <password>', 'Loki password')
+  .option('--hostname <hostname>', 'URL for Loki')
+  .option('-b, --batch', 'Should logs be sent in batch mode')
+  .option('-i, --interval <interval>', 'The interval at which batched logs are sent in seconds')
+  .option('-t, --timeout <timeout>', 'Timeout for request to Loki')
+  .option('-s, --silenceErrors', 'If false, errors will be displayed in the console')
+  .option('-r, --replaceTimestamp', 'Replace pino logs timestamps with Date.now()')
+  .option('-l, --labels <label>', 'Additional labels to be added to all Loki logs')
+  .option(
+    '-pl, --propsLabels <labels>',
+    'Fields in log line to convert to Loki labels (comma separated values)',
+  )
+  .option('--no-stdout', 'Disable output to stdout')
 
 /**
  * Create a PinoLokiOptionsContract from cli arguments
  */
 export const createPinoLokiConfigFromArgs = () => {
-  const opts = parseArgs()
+  const opts = program.parse(process.argv).opts()
 
   const config: LokiOptions = {
     host: opts.hostname,
