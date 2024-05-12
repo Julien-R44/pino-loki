6d76363b4948e5628d9ef0e91775fb2255dbc21c
Julien Ripouteau <julien@ripouteau.com>
2024-05-12 11:14:17 +0200

    chore: ignore adonisjs from eslint

--- eslint.config.js
+++ eslint.config.js
@@ -1,4 +1,9 @@
 // eslint-disable-next-line @typescript-eslint/no-var-requires
 const { julr } = require('@julr/tooling-configs/eslint')
 
-module.exports = julr()
+module.exports = julr(
+  {},
+  {
+    ignores: ['examples/adonisjs/**/*'],
+  },
+)
